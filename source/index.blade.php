583a559779fda13e68342aa94d32af9dfcecec87
Sbhargava <shivzb@gmail.com>
2021-01-04 20:16:38 -0500

    reverting layout commit

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,9 +1,11 @@
+@extends('_layouts.master')
 
+@section('content')
     <h1>TheRightMargin</h1>
 
     <p>Unfortunately, we shut down TheRightMargin effective Dec. 20th.</p>
 
     <p>If you were a user of TheRightMargin, thank you for trying us out. Best of luck on your writing journey.</p>
 
-    
 
+@endsection
