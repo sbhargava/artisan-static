7670116fb26075c466ec1a4818592113d05fb05d
Sbhargava <shivzb@gmail.com>
2021-01-04 20:13:54 -0500

    Get rid of header/footer

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,6 +1,4 @@
-@extends('_layouts.master')
 
-@section('content')
     <h1>TheRightMargin</h1>
 
     <p>Unfortunately, we shut down TheRightMargin effective Dec. 20th.</p>
@@ -8,4 +6,4 @@
     <p>If you were a user of TheRightMargin, thank you for trying us out. Best of luck on your writing journey.</p>
 
     
-@endsection
+
